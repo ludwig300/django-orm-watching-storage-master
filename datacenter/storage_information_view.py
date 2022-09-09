fd2a3b8191da32e918ba813cd554b6400d5ab1f7
ludwig300 <105148929+ludwig300@users.noreply.github.com>
2022-09-09 18:25:25 +0700

    Edited by PEP8

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -1,4 +1,4 @@
-from django.utils.timezone import localtime 
+from django.utils.timezone import localtime
 from django.shortcuts import render, get_list_or_404
 from datacenter.models import Visit, get_duration, format_duration
 
@@ -6,7 +6,7 @@ from datacenter.models import Visit, get_duration, format_duration
 def storage_information_view(request):
     visit = get_list_or_404(Visit, leaved_at__isnull=True)
     non_closed_visits = list()
-    for some_visit in visit:   
+    for some_visit in visit:
         entered_at = localtime(some_visit.entered_at)
         duration = format_duration(get_duration(some_visit))
         who_entered = some_visit.passcard
@@ -14,15 +14,10 @@ def storage_information_view(request):
             {
                 'who_entered': who_entered,
                 'entered_at': entered_at,
-                'duration': duration,
+                'duration': duration
             }
         )
     context = {
-        'non_closed_visits': non_closed_visits,  # не закрытые посещения
+        'non_closed_visits': non_closed_visits
     }
     return render(request, 'storage_information.html', context)
-
-
-
-
-    
